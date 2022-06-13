edb2c53bdb7df7e6a937dfbabe88fc96a8343e0a
Kym Reilly <kymreilly@hotmail.com>
2022-06-13 14:10:19 +0930

    altered prettier to prevent syntax autocorrect errors

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,8 @@
 const sequelize = require("../config/connection");
 const { User, Post } = require("../models");
 
-const userData = require("./userData.json");
-const postData = require("./postData.json");
+const userData = require("./userSeed.json");
+const postData = require("./postSeed.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
