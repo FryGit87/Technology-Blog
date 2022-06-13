763b31fef69d8debaeeddf32436f3612c5ddafc3
Kym Reilly <kymreilly@hotmail.com>
2022-06-13 16:31:04 +0930

    css and blog creation implemented

--- models/Post.js
+++ models/Post.js
@@ -1,5 +1,5 @@
-const { Model, DataTypes } = require("sequelize");
-const sequelize = require("../config/connection");
+const { Model, DataTypes } = require('sequelize')
+const sequelize = require('../config/connection')
 
 class Post extends Model {}
 
@@ -27,8 +27,8 @@ Post.init(
     user_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: "user",
-        key: "id",
+        model: 'user',
+        key: 'id',
       },
     },
   },
@@ -36,8 +36,8 @@ Post.init(
     sequelize,
     freezeTableName: true,
     underscored: true,
-    modelName: "post",
+    modelName: 'post',
   }
-);
+)
 
-module.exports = Post;
+module.exports = Post
--- public/js/profile.js
+++ public/js/profile.js
@@ -1,46 +1,45 @@
 const newFormHandler = async (event) => {
-  event.preventDefault();
+  event.preventDefault()
 
-  const name = document.querySelector("#post-name").value.trim();
-  const description = document.querySelector("#post-desc").value.trim();
+  const title = document.querySelector('#post-name').value.trim()
+  // const needed_funding = document.querySelector('#post-funding').value.trim();
+  const post_content = document.querySelector('#post-desc').value.trim()
 
-  if (name && needed_funding && description) {
+  if (title && post_content) {
     const response = await fetch(`/api/posts`, {
-      method: "POST",
-      body: JSON.stringify({ name, description }),
+      method: 'POST',
+      body: JSON.stringify({ title, post_content }),
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
-    });
+    })
 
     if (response.ok) {
-      document.location.replace("/profile");
+      document.location.replace('/profile')
     } else {
-      alert("Failed to create post");
+      alert('Failed to create post')
     }
   }
-};
+}
 
 const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute("data-id")) {
-    const id = event.target.getAttribute("data-id");
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id')
 
     const response = await fetch(`/api/posts/${id}`, {
-      method: "DELETE",
-    });
+      method: 'DELETE',
+    })
 
     if (response.ok) {
-      document.location.replace("/profile");
+      document.location.replace('/profile')
     } else {
-      alert("Failed to delete post");
+      alert('Failed to delete post')
     }
   }
-};
+}
 
 document
-  .querySelector(".new-post-form")
-  .addEventListener("submit", newFormHandler);
+  .querySelector('.new-post-form')
+  .addEventListener('submit', newFormHandler)
 
-document
-  .querySelector(".post-list")
-  .addEventListener("click", delButtonHandler);
+document.querySelector('.post-list').addEventListener('click', delButtonHandler)
