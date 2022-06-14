c4c7a51a63e7ea9605ab540868a388ffa765e65b
Kym Reilly <kymreilly@hotmail.com>
2022-06-14 23:27:26 +0930

    comments displayed, still require design elements

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { Post, User } = require('../models')
+const { Post, User, Comment } = require('../models')
 const withAuth = require('../utils/auth')
 
 router.get('/', async (req, res) => {
@@ -37,11 +37,25 @@ router.get('/post/:id', async (req, res) => {
         },
       ],
     })
+    const commentData = await Comment.findAll({
+      where: {
+        post_id: req.params.id,
+      },
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    })
 
     const post = postData.get({ plain: true })
-
+    const comments = commentData.map((comment) => comment.get({ plain: true }))
+    // console.log(post)
+    // console.log(comments)
     res.render('post', {
       ...post,
+      comments,
       logged_in: req.session.logged_in,
     })
   } catch (err) {
