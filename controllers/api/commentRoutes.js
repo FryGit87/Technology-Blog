a44431637e3c95d61f54f1dbd5c9628812a4aa6e
Kym Reilly <kymreilly@hotmail.com>
2022-06-21 12:06:55 +0930

    add update control for comment and post

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -6,7 +6,7 @@ router.post('/', withAuth, async (req, res) => {
   try {
     const newComment = await Comment.create({
       ...req.body,
-      user_id: req.session.user_id, 
+      user_id: req.session.user_id,
     })
 
     res.status(200).json(newComment)
@@ -32,6 +32,19 @@ router.delete('/:id', withAuth, async (req, res) => {
     res.status(200).json(commentData)
   } catch (err) {
     res.status(500).json(err)
+  }
+})
+
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const commentData = await Comment.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    })
+    res.status(200).json(commentData)
+  } catch (err) {
+    res.status(400).json(err)
   }
 })
 
